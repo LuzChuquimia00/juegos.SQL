1565fd08eea9c82439fcdea56fad0f8703aba95d
LuzChuquimia <luzchuquimia1680@gmail.com>
2024-03-27 11:51:12 -0300

    ACTUALIZADO

--- Ejercicios/Ejercicios2.0.sql
+++ Ejercicios/Ejercicios2.0.sql
@@ -1,6 +1,13 @@
 --ACT1--
 SELECT FirstName,LastName FROM employees 
-ORDER BY FirstName AND LastName DESC
+ORDER BY FirstName AND LastName DESC;
 --ACT2--
-SELECT FirstName,ReportsTo FROM employees WHERE ReportsTo IS NULL 
+SELECT FirstName,ReportsTo FROM employees WHERE ReportsTo IS NULL;
 --ACT3--
+SELECT billingaddress, total FROM invoices
+ORDER BY total DESC
+LIMIT 5;
+--ACT4--
+SELECT t.name,t.AlbumId, t.Milliseconds FROM tracks t
+WHERE t.AlbumId LIKE '5'; 
+
