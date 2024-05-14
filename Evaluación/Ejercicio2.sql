d9b92b201650504689ae92bf237b98dd7a1e5d55
LuzChuquimia <luzchuquimia1680@gmail.com>
2024-05-14 16:57:10 -0300

    EJERCICIO2

--- Evaluación/Ejercicio2.sql
+++ Evaluación/Ejercicio2.sql
@@ -2,9 +2,9 @@
 -- (marca, modelo y patente) de todos los
 -- varones más de 70 años, elegir el orden
 -- en que se muestran--
-SELECT p.name AS nombrePersonas, d.car_make AS marca, d.car_model AS modelo, d.plate_number AS patente, d.age AS edad
+SELECT p.name AS nombrePersonas, d.car_make AS marca, d.car_model AS modelo, d.plate_number AS patente, d.age AS edad, d.gender AS genero
 FROM person p 
 JOIN drivers_license d ON p.license_id = d.id
-WHERE d.age > 70;
-
+WHERE d.age > 70 and d.gender LIKE 'male'
+ORDER BY d.age DESC;
 
