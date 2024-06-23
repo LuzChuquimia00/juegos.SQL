691419832a64ab0bb7540637e8c6dd561980db22
LuzChuquimia <luzchuquimia1680@gmail.com>
2024-06-23 18:08:33 -0300

    corregir la 4 y completar 5,6,7

--- 2doCuatrimestre-CHINOOK/Ejercicios.sql
+++ 2doCuatrimestre-CHINOOK/Ejercicios.sql
@@ -4,8 +4,12 @@
 UPDATE employees SET phone =  fax;
 --4.Borrar todos los géneros que tengan menos de 50 canciones
 --(borrar todo lo necesario para poder borrar estos géneros)
-DELETE FROM tracks WHERE (SELECT g.name AS GenerosDelasCaciones, count(t.TrackId) AS Canciones 
-							FROM genres g
-							JOIN tracks t ON g.GenreId = t.GenreId
-							GROUP BY g.GenreId
-							HAVING  count(t.TrackId) < 50);
+
+DELETE FROM genres
+WHERE GenreId IN (
+    SELECT g.GenreId
+    FROM genres g
+    JOIN tracks t ON g.GenreId = t.GenreId
+    GROUP BY g.GenreId
+    HAVING COUNT(t.TrackId) < 50
+);
